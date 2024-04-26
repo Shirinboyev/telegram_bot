98a5092bfe04f9b2e5fdf51722e265c526e7b1f8
Gayrat <gayratshirinboyev6707@gmail.com>
2024-04-26 18:27:05 +0500

    start

---
+++ src/main/java/org/example/MyBot.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        try {
+
+
+
+            Long myChatid = 1712225965L;
+            SendMessage sendMessage = new SendMessage();
+            execute(sendMessage);
+
+
+
+
+        } catch (TelegramApiException e) {
+        }
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "shirinboyevtestbot";
+    }
+    @Override
+    public String getBotToken() {
+        return "7065868832:AAG8VwBarwl-R-c5QVnxCjHfBMqV3Htk7h8";
+    }
+}
