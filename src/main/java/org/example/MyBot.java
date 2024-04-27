f1ad6880c6b2f0ccb5a7c7d2c2e4b8370cc9b60c
Gayrat <gayratshirinboyev6707@gmail.com>
2024-04-27 09:07:55 +0500

    Reply Keyword Button

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -4,6 +4,9 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
@@ -14,53 +17,27 @@ public class MyBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         try {
-            String text = update.getMessage().getText();
             Long chatId = update.getMessage().getChatId();
 
-            TelegramState currentUser = null;
-            for (TelegramState user : users) {
-                if (user.getChatId().equals(chatId)) {
-                    currentUser = user;
-                }
-            }
-            if(currentUser == null) {
-                currentUser = new TelegramState();
-                currentUser.setChatId(chatId);
-                users.add(currentUser);
-            }
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(chatId);
+            sendMessage.setText("Button");
 
+            List<KeyboardRow> rows = new ArrayList<>();
+            ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+            replyKeyboardMarkup.setKeyboard(rows);
 
-            if(text.equals("/start")){
-                SendMessage sendMessage = new SendMessage();
-                sendMessage.setText("Assalomu alekum! Botga xush kelibsiz\nIltimos ismingizni kiriting 😌");
-                sendMessage.setChatId(chatId);
-                execute(sendMessage);
-                currentUser.setSTATE(UserState.ENTER_FIRSTNAME);
-            } else {
-                if (currentUser.getSTATE().equals(UserState.ENTER_FIRSTNAME)) {
-                    currentUser.setName(text);
-                    SendMessage sendMessage = new SendMessage();
-                    sendMessage.setText("Iltimos familiyangizni kiriting 😌");
-                    sendMessage.setChatId(chatId);
-                    execute(sendMessage);
-                    currentUser.setSTATE(UserState.ENTER_LASTNAME);
-                } else if (currentUser.getSTATE().equals(UserState.ENTER_LASTNAME)) {
-                    currentUser.setName(text);
-                    SendMessage sendMessage = new SendMessage();
-                    sendMessage.setText("Siz ro'yxatdan o'tdingiz 🥳\n" +
-                                        "Sizga qanday yordam bera olamiz");
-                    sendMessage.setChatId(chatId);
-                    execute(sendMessage);
-                    currentUser.setSTATE(UserState.REGISTERED);
-                } else if (currentUser.getSTATE().equals(UserState.REGISTERED)) {
-                    SendMessage sendMessage = new SendMessage();
-                    sendMessage.setText("Iltimos Admin Javobini Kuting 😌");
-                    System.out.println("Lekin senga hech kim javob bermaydi 🤣🤣🤣 ");
-                    sendMessage.setChatId(chatId);
-                    execute(sendMessage);
-                }
-            }
-        } catch (TelegramApiException e) {
+            KeyboardButton button = new KeyboardButton();
+
+            KeyboardRow row = new KeyboardRow();
+            row.add(button);
+            button.setText("Musiqa");
+            rows.add(row);
+
+            sendMessage.setReplyMarkup(replyKeyboardMarkup);
+            execute(sendMessage);
+
+        } catch (Exception e) {
         }
     }
 
